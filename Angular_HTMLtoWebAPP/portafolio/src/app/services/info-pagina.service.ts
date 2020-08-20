cc7fb43894dffde9466e1b33c1a2e79d996112b8
Mtllns <miquelmtllns@pm.me>
2020-08-20 16:16:29 +0200

    Carga info de Firebase a la página

--- Angular_HTMLtoWebAPP/portafolio/src/app/services/info-pagina.service.ts
+++ Angular_HTMLtoWebAPP/portafolio/src/app/services/info-pagina.service.ts
@@ -10,10 +10,16 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) {
 
-    console.log('Info Pagina cargada');
+    this.cargarInfo();
+    this.cargarEquipo();
 
+  }
+
+  private cargarInfo() {
     // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
         .subscribe( (resp: InfoPagina) => {
@@ -22,7 +28,14 @@ export class InfoPaginaService {
           this.info = resp;
           // console.log(resp);
       });
-
-
   }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-html-a8b93.firebaseio.com/equipo.json')
+        .subscribe( (resp: any[]) => {
+          this.equipo = resp;
+          console.log(resp);
+        });
+  }
+
 }
