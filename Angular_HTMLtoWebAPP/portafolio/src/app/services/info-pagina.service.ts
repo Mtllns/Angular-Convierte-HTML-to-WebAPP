4aa62af0980bedfc7c7d037e4a84a9ac21a0b829
Mtllns <miquelmtllns@pm.me>
2020-08-20 17:01:07 +0200

    Creando servicio productos

--- Angular_HTMLtoWebAPP/portafolio/src/app/services/info-pagina.service.ts
+++ Angular_HTMLtoWebAPP/portafolio/src/app/services/info-pagina.service.ts
@@ -34,7 +34,7 @@ export class InfoPaginaService {
     this.http.get('https://angular-html-a8b93.firebaseio.com/equipo.json')
         .subscribe( (resp: any[]) => {
           this.equipo = resp;
-          console.log(resp);
+          // console.log(resp);
         });
   }
 
